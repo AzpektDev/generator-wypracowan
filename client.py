bc944f2aebe33e65019f0c84e1d453c3acc03cb3
Azpekt <65199167+AzpektDev@users.noreply.github.com>
2023-04-19 12:30:59 +0200

    revert

--- client.py
+++ client.py
@@ -1,23 +1,26 @@
 import socket
 
 host = socket.gethostname()
-port = 10001
+port = 10002
 
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientsocket.connect((host, port))
 
 # data = "Hello, server!"
 data = input("give temat: ")
+data = data + "\r\n\r\n-EndStream-"
 clientsocket.send(data.encode('utf-8'))
 
-
-response = clientsocket.recv(1024).decode('utf-8')
+response = b''
+while b'\r\n\r\n-EndStream-' not in response:
+    response += clientsocket.recv(1024)
+response = response.decode('utf-8')
+response = response.replace("\r\n\r\n-EndStream-", "")
 print(f"Response from server: {response}")
 
 
 with open("server_response.txt", "w") as f:
     f.write(response)
-
 
 clientsocket.close()
 
--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 import socket
-
 import openai
 
 openai.api_key = "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"
 
 host = socket.gethostname()
-port = 10001
+port = 10002
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind((host, port))
@@ -18,7 +17,12 @@ while True:
     clientsocket, addr = serversocket.accept()
 
     print(f"Connection from {addr} has been established.")
-    data = clientsocket.recv(1024).decode('utf-8')
+    data = b''
+    while b'\r\n\r\n-EndStream-' not in data:
+        data += clientsocket.recv(1024)
+    data = data.decode('utf-8')
+    data = data.replace("\r\n\r\n-EndStream-", "")
+
     print(f"Data received from client: {data}")
 
     response = openai.Completion.create(
@@ -28,6 +32,9 @@ while True:
         temperature=0.7
     )
 
-    clientsocket.send(response['choices'][0]['text'].encode('utf-8'))
+    content_length = len(response['choices'][0]['text'])
+    response = response['choices'][0]['text'] + "\r\n\r\n-EndStream-"
+
+    clientsocket.send(response.encode('utf-8'))
 
     clientsocket.close()
